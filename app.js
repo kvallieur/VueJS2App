57dfd59ce90cfbf3e8cc50d0dce6691d9fd5bee3
kvallieur <kvallieur@gmail.com>
2018-03-13 21:33:42 -0500

    Day 2 Learnings

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 new Vue({
-    el: '#vue-app',
-    data: {
-        name: 'Kare'
-    },
-    methods: {
-      greet: function(time){
-        console.log(`Method begin ${time}`);
-        return "Good Bad Ugly" + time;
-      }
+  el: '#vue-app',
+  data: {
+    name: 'Kare'
+  },
+  methods: {
+    greet: function(time) {
+      return "Good Bad Ugly" + time;
     }
+  }
 });
---
+++ dataBind/dataBind.js
@@ -0,0 +1,9 @@
+new Vue({
+  el: '#vue-app',
+  data: {
+    name: 'Karthik',
+    url: 'https://www.google.com/',
+    age: 45,
+    htmlContent: '<h1> This is some html </h1>'
+  }
+});
